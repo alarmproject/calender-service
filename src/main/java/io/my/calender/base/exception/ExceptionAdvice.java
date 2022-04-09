a14751b77f9b8b8af64796ff2bba10a8475ee782
mysend12 <mysend12@naver.com>
2022-04-09 11:24:21 +0900

    예외 핸들링 반환타입 수정

--- src/main/java/io/my/calender/base/exception/ExceptionAdvice.java
+++ src/main/java/io/my/calender/base/exception/ExceptionAdvice.java
@@ -1,19 +1,20 @@
 package io.my.calender.base.exception;
 
 import io.my.calender.base.payload.BaseResponse;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.reactive.function.server.EntityResponse;
-import reactor.core.publisher.Mono;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
 
     @ExceptionHandler(Exception.class)
-    protected Mono<EntityResponse<BaseResponse>> exceptionAdvice(Exception e) {
+    protected ResponseEntity<BaseResponse> exceptionAdvice(Exception e) {
         e.printStackTrace();
-        return EntityResponse.fromObject(new BaseResponse(1, "Server Error")).status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        return ResponseEntity.internalServerError().body(
+                new BaseResponse(
+                        ErrorTypeEnum.SERVER_ERROR.getCode(),
+                        ErrorTypeEnum.SERVER_ERROR.getResult()));
     }
 
 
