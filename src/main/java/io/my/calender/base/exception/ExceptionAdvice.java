16b27800ffcbf3ca40ac36e06ff34713dbb67f62
mysend12 <mysend12@naver.com>
2022-05-29 17:16:16 +0900

    예외 발생 시, slack으로 전송 추가

--- src/main/java/io/my/calender/base/exception/ExceptionAdvice.java
+++ src/main/java/io/my/calender/base/exception/ExceptionAdvice.java
@@ -2,15 +2,20 @@ package io.my.calender.base.exception;
 
 import io.my.calender.base.exception.object.DatabaseException;
 import io.my.calender.base.payload.BaseResponse;
+import io.my.calender.slack.SlackService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ExceptionAdvice {
+    private final SlackService slackService;
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<BaseResponse> exceptionAdvice(Exception e) {
+        slackService.sendSlackException(e);
         e.printStackTrace();
         return ResponseEntity.internalServerError().body(
                 new BaseResponse(
@@ -20,6 +25,7 @@ public class ExceptionAdvice {
 
     @ExceptionHandler(DatabaseException.class)
     protected ResponseEntity<BaseResponse> exceptionAdvice(DatabaseException e) {
+        slackService.sendSlackException(e);
         return ResponseEntity.internalServerError()
                 .body(new BaseResponse(
                         ErrorTypeEnum.DATABASE_EXCEPTION.getCode(),
